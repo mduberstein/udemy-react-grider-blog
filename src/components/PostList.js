7ca5f115bbd2df0060a924bbd9b5c7c2c202b954
Michael Duberstein <dubermic@flutterint.com>
2022-08-27 20:11:57 -0400

    After Clip 252 Wiring Up an Action Creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
-  render () {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+// null as first argument means we don't have mapStateToProps function yet,
+// i.e. for now we are not passing any state to the component
+// i.e. we are only passing action creators to the component via the second argument
+export default connect(null, { fetchPosts })(PostList);
