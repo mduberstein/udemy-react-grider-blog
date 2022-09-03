403c1355bd8a3a7061052bf99ac5f36ed0fb8a8e
Michael Duberstein <dubermic@flutterint.com>
2022-09-03 18:20:26 -0400

    After Clip: 267 Switch Statements in Reducers

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,3 +1,16 @@
-export default () => {
-  return 123;
+const postReducer = (state = [], action) => {
+  //// ALT1
+  // if(action.type === 'FETCH_POSTS') {
+  //   return action.payload;
+  // }
+  // return state;
+
+  switch(action.type){
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 };
+
+export default postReducer;
