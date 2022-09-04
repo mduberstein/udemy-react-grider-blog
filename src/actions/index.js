0b43072fa6b3205ca3d0ca0fe75045b32b8e49b1
Michael Duberstein <dubermic@flutterint.com>
2022-09-04 17:55:44 -0400

    In Clip 269: Dispatching Correct Values

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,8 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => 
   async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-    dispatch({type: 'FETCH_POSTS', payload: response});
-  }
+  //  Typical error, dispatching the entire response
+  dispatch({type: 'FETCH_POSTS', payload: response});
+  // Correcttion of typical error:
+  // dispatch({type: 'FETCH_POSTS', payload: response.data});
+  };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,6 +8,7 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
@@ -15,4 +16,11 @@ class PostList extends React.Component {
 // null as first argument means we don't have mapStateToProps function yet,
 // i.e. for now we are not passing any state to the component
 // i.e. we are only passing action creators to the component via the second argument
-export default connect(null, { fetchPosts })(PostList);
+// export default connect(null, { fetchPosts })(PostList);
+
+// Version of Clip 269
+const mapStateToProps = (state) => {
+  return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
