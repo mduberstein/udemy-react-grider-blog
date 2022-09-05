30583dee28e7903a2432cd114e5ea9c390319ca0
Michael Duberstein <dubermic@flutterint.com>
2022-09-04 20:56:54 -0400

    In Clip 269: Dispatching Correct Values, after correction

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const fetchPosts = () =>
   async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
   //  Typical error, dispatching the entire response
-  dispatch({type: 'FETCH_POSTS', payload: response});
-  // Correcttion of typical error:
-  // dispatch({type: 'FETCH_POSTS', payload: response.data});
+  // dispatch({type: 'FETCH_POSTS', payload: response});
+  // Correction of typical error:
+  dispatch({type: 'FETCH_POSTS', payload: response.data});
   };
