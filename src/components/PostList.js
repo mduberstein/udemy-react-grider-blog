0b7409f2dfc68a880f269716151077c99152c94c
Michael Duberstein <dubermic@flutterint.com>
2022-09-06 19:59:45 -0400

    After Clip 274 Finding Relevant Users

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -26,7 +26,7 @@ class PostList extends React.Component {
   }
 
   render() {
-    console.log(this.props.posts);
+    // console.log(this.props.posts);
     return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
