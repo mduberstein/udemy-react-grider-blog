a32b3dc1a57b390b92cc649b41fa6ab4db9c7fa0
Michael Duberstein <dubermic@flutterint.com>
2022-09-06 16:41:52 -0400

    After Clip 272 Fetching Singular Records

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => 
-  async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
   //  Typical error, dispatching the entire response
   // dispatch({type: 'FETCH_POSTS', payload: response});
   // Correction of typical error:
-  dispatch({type: 'FETCH_POSTS', payload: response.data});
-  };
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch( {type: 'FETCH_USER', payload: response.data})
+};
