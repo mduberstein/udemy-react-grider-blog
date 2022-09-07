1fa5f706ec45725f3ee73ebb71f82dea6e05e265
Michael Duberstein <dubermic@flutterint.com>
2022-09-06 22:04:25 -0400

    After Clip 278 Memoization issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -8,7 +9,14 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-  dispatch( {type: 'FETCH_USER', payload: response.data})
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch( {type: 'FETCH_USER', payload: response.data})
+// };
+
+export const fetchUser = function(id) {
+  return _.memoize(async function (dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch( {type: 'FETCH_USER', payload: response.data})
+  });
 };
